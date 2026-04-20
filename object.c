27ae79489eac0c4b56540ebbeeb033710ee4e3bd
Taran S <taranshivanna@gmail.com>
2026-04-20 10:32:50 +0530

    Phase 1: increase tmp_path buffer size to fix truncation warning

--- object.c
+++ object.c
@@ -126,7 +126,7 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
     // Step 6: Write to temp file
     char final_path[512];
     object_path(id_out, final_path, sizeof(final_path));
-    char tmp_path[512];
+    char tmp_path[600];
     snprintf(tmp_path, sizeof(tmp_path), "%s.tmp", final_path);
 
     int fd = open(tmp_path, O_CREAT | O_WRONLY | O_TRUNC, 0644);
